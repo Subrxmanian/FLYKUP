c0ec7d55926bec7025e3c7b7035d2bd1ebafceed
Subramanian M <168618150+Subrxmanian@users.noreply.github.com>
2025-03-10 19:54:58 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,16 @@
+
+const Config = {
+  "AWS_REGION": "ap-southeast-2",
+  "AWS_ACCESSKEYID": "AKIAU72LF366ZCAENH4B",
+  "AWS_SECRETACCESSKEY": "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",
+  "AWS_BUCKET": "flykup-reel-video"
+}
+
+export const GOOGLE_CLIENT_ID = '1052124710349-d7dgcjaqopq30de4g17te2gj6cpgv8v9.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_SECRET = "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"
+export const GOOGLE_REDIRECT_URI = 'http://localhost:5173/auth/register'
+export const FACEBOOK_APP_ID = '594964216846874'
+export const FACEBOOK_APP_SECRET = '300da59e0b10f8e107dd90e7639cc9d5'
+export default Config
+export const socketurl = "https://flykup-be-new.onrender.com"
+export const webClientId ='383052907259-dn2vjvo5sctdt5pkcgntp6h45ev4c8s7.apps.googleusercontent.com'
